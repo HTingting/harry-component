62dcbff5c5b23c74805386c018a43b7fb9fcf45d
2668391473@qq.com <Ht@18211276722>
2021-01-26 17:38:35 +0800

    更新搜索组件2

--- src/components/searchTool/FormItem.vue
+++ src/components/searchTool/FormItem.vue
@@ -83,76 +83,8 @@
 </template>
 
 <script>
-// elementui日期时间范围 快捷选项
-const pickerOptionsRange = {
-    shortcuts: [
-        {
-            text: '今天',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date(new Date().toDateString())
-                start.setTime(start.getTime())
-                picker.$emit('pick', [start, end])
-            }
-        }, {
-            text: '最近一周',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date()
-                start.setTime(end.getTime() - 3600 * 1000 * 24 * 7)
-                picker.$emit('pick', [start, end])
-            }
-        }, {
-            text: '最近一个月',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date()
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
-                picker.$emit('pick', [start, end])
-            }
-        }, {
-            text: '最近三个月',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date()
-                start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
-                picker.$emit('pick', [start, end])
-            }
-        }
-    ]
-}
+import { pickerOptionsRange, pickerOptionsRangeMonth } from "@/utils/tools";
 
-// elementui月份范围 快捷选项
-const pickerOptionsRangeMonth = {
-    shortcuts: [
-        {
-            text: '今年至今',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date(new Date().getFullYear(), 0)
-                picker.$emit('pick', [start, end])
-            }
-        },
-        {
-            text: '最近半年',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date()
-                start.setMonth(start.getMonth() - 6)
-                picker.$emit('pick', [start, end])
-            }
-        },
-        {
-            text: '最近一年',
-            onClick (picker) {
-                const end = new Date()
-                const start = new Date()
-                start.setMonth(start.getMonth() - 12)
-                picker.$emit('pick', [start, end])
-            }
-        }
-    ]
-}
 export default {
     inheritAttrs: false,
     name: "FormItem",
